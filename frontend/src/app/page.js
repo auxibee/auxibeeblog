cb856d8dd9026257fbe2e7b20300a96153fdbf02
Yaw Twumasi Acheampong <auxibee@gmail.com>
2023-12-16 11:41:16 -0500

    add

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,19 +1,10 @@
-"use client"
 
-import { useEffect, useState } from "react"
 
 export default function Home() {
-  const [user, setUser] = useState()
-  useEffect(async () => {
-    const response = await fetch('http://localhost:8000/api/names')
-    const t = await response.json()
-    setUser(t)
 
-  }, [])
   return (
     <main>
       Website is under construction by
-      <p>{user ? `Your data: ${user.username}` : 'Loading...'}</p>
     </main>
   )
 }
