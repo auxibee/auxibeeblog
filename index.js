acfdc8084c8d0ad392a5bd64f4a7b28a452d1191
Yaw Twumasi Acheampong <auxibee@gmail.com>
2023-12-15 21:07:43 -0500

    dd

--- index.js
+++ index.js
@@ -6,5 +6,9 @@ app.get('/', async (req, res) => {
     res.json({ message: 'Website is under construction.....' })
 })
 
+app.get('/names', async (req, res) => {
+    res.json({ username: 'yaw twumasi', email: 'auxibee@gmail.com' })
+})
+
 
 app.listen(8080, "0.0.0.0", () => console.log('server started listening'))
